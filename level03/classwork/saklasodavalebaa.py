f97685f4e477063d49fe48d69a6f14e72b59dc99
andriamosikashvili <andrushkamartushka@gmail.com>
2025-07-23 11:04:38 +0400

    “განახლება”

---
+++ level03/classwork/saklasodavalebaa.py
@@ -0,0 +1,16 @@
+int=6
+float=2.5
+print(6+2.5)
+
+name="andria"
+lastname="mosikashvili"
+print(name + lastname)
+
+print(3+1)
+print(2.5+1)
+print(2-1)  
+print(12.5-5.5)
+print(5*2)
+print(5*12.2)
+print(2/2)
+print(9//1)
