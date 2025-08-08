2a3a92b092f285c256691c7dd53cbf3beeac7631
andriamosikashvili <andrushkamartushka@gmail.com>
2025-08-08 17:35:53 +0400

    “განახლება”

---
+++ level05/homework/davaleba.py
@@ -0,0 +1,56 @@
+#კომენტარები გამოიყენება სხვა ადამიანებისთვის, რომ გაიგონ სად რა გააკეთე
+
+na_me="andria"
+last_name="mosikashvili"
+ag_e=12
+fav_game="minecraft"
+my_phone="iphone eleven"
+fav_subject="english"
+cit_y="tbilisi"
+home_country="georgia"
+hobb_y="football"
+fav_color="black"
+
+print(na_me)
+print(last_name)
+print(ag_e)
+print(fav_game)
+print(my_phone)
+print(fav_subject)
+print(cit_y)
+print(home_country)
+print(hobb_y)
+print(fav_color)
+
+
+name="luka"
+age_num=56
+my_favmov="blood and bone"
+my_friends_name="nika"
+
+print("my name is" +name)
+print("my age is" + age_num)
+print("my fav mov is" + my_favmov)
+print("my friends name is" + my_friends_name)
+
+name="andria"
+sur_name="mosikashvili" 
+print(name + sur_name)
+
+name_2="sandro"
+sur_name2="ivelashvili"
+print(name_2 + sur_name)
+
+word=("human")
+name_3("kako")
+print(name_3+word)
+
+word1="skibidi"
+word2="toilet"
+print(word1 + word 2)
+
+word_3="pretty"
+word_4="girl"
+print(word_3 + word_4)
+
+#snake case- არის როდესაც ერთი სიტყვა მეორესგან დაშორებული არის ქვედატირით(underscore) ეს გამოიყენება ცვლადებში რადგან ჩვენ თუ დავწერთ space-ს ეს შეცდომა გამოდის
