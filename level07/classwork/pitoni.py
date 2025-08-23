0f687b78bdda4a048a1f96fbec82684580e11620
andriamosikashvili <andrushkamartushka@gmail.com>
2025-08-23 19:58:38 +0400

    “განახლება”

---
+++ level07/classwork/pitoni.py
@@ -0,0 +1,34 @@
+#input არის შემომავალი ინფორმაცია
+#output არის გამომავალი ინფორმაცია
+#კონკანტინაცია არის ორი სტრინგი რიცხვების შეერთება
+num=input()
+print(num)
+
+name=input()
+lastname=input()
+age=input()
+home=input()
+favcolor=input()
+favcar=input()
+print(name)
+print(lastname)
+print(age)
+print(home)
+print(favcolor)
+print(favcar)
+
+int("5")
+int("7")
+int("8")
+int("10")
+int("19")
+
+str(10)
+str(20)
+str(160)
+str(1)
+str(190)
+
+num1=str(input())
+print(num1 + 15)
+
---
+++ level07/homework/Pitonii.py
@@ -0,0 +1,18 @@
+#input-არის  შემავალი ინფორმაცია
+#output გამომავალი ინფორმაცია
+ 
+#კონკანტინაცია არის ორი სტრინგის შერევა მათემატიკის სახით
+
+num1="6"
+num2=7
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(type(float("67")))
+
+
+
+
+
